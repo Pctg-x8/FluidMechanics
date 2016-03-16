a1daa3fbbe6fb7bedca38e80eb57273b936b822d
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-03-16 18:13:15 +0900

    make ModID, Name and Version to object constant

--- src/FMEntry.scala
+++ src/FMEntry.scala
@@ -4,13 +4,17 @@ import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod._
 import cpw.mods.fml.common.event._
 
-@Mod(modid="com.cterm2.mcfm1710", name="Fluid Mechanics", version="1.0-alpha", modLanguage="scala")
+@Mod(modid=FMEntry.ID, name=FMEntry.Name, version=FMEntry.Version, modLanguage="scala")
 object FMEntry
 {
+	final val ID = "com.cterm2.mcfm1710"
+	final val Name = "Fluid Mechanics"
+	final val Version = "1.0-alpha"
+	
 	@EventHandler
 	def preInit(e: FMLPreInitializationEvent) =
 	{
-		System.out.println("Fluid Mechanics version 1.0-alpha.")
+		System.out.println(Name + " version " + Version + ".")
 	}
 	@EventHandler
 	def init(e: FMLInitializationEvent) =
