4d59b3750e25600a0bd151e2e1a472cf55b0051b
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-03-26 20:21:04 +0900

    add texture graphs

---
+++ src/main/resources/assets/mcfm1710/textures/blocks/split.sh
@@ -0,0 +1,3 @@
+convert source/thermalGenerator.png -crop 7x1@ +repage +adjoin thermalGenerator%d.png
+convert source/thermalFluidGenerator.png -crop 7x1@ +repage +adjoin thermalFluidGenerator%d.png
+
