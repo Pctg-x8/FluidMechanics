d2a52e4df94f764e3905d4c052176fb40df76fb6
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-04-27 14:46:23 +0900

    changed tag record to based on type classes

--- src/utils/activeNBTRecord.scala
+++ src/utils/activeNBTRecord.scala
@@ -9,12 +9,32 @@ object ActiveNBTRecord
 	implicit class Record(val tag: NBTTagCompound) extends AnyVal
 	{
 		// Setters
-		def update(key: String, value: Byte) = tag.setByte(key, value)
-		def update(key: String, value: Short) = tag.setShort(key, value)
-		def update(key: String, value: Int) = tag.setInteger(key, value)
-		def update(key: String, value: NBTTagCompound) = tag.setTag(key, value)
+		def update[T](key: String, value: T)(implicit setterImpl: SetterImpl[T]) = setterImpl.set(tag, key, value)
+		def update[T](key: String, value: Option[T])(implicit setterImpl: SetterImpl[T]) = value foreach { setterImpl.set(tag, key, _) }
 		// Getters
 		def apply[T](key: String)(implicit getterImpl: GetterImpl[T]) = getterImpl.get(tag, key)
+	}
+
+	// Type Classes //
+	sealed trait SetterImpl[@specialized T]
+	{
+		def set(tag: NBTTagCompound, key: String, value: T): Unit
+	}
+	implicit object ByteSetter extends SetterImpl[Byte]
+	{
+		def set(tag: NBTTagCompound, key: String, value: Byte) = tag.setByte(key, value)
+	}
+	implicit object ShortSetter extends SetterImpl[Short]
+	{
+		def set(tag: NBTTagCompound, key: String, value: Short) = tag.setShort(key, value)
+	}
+	implicit object IntSetter extends SetterImpl[Int]
+	{
+		def set(tag: NBTTagCompound, key: String, value: Int) = tag.setInteger(key, value)
+	}
+	implicit object TagSetter extends SetterImpl[NBTTagCompound]
+	{
+		def set(tag: NBTTagCompound, key: String, value: NBTTagCompound) = tag.setTag(key, value)
 	}
 
 	sealed trait GetterImpl[@specialized T]
